1d2d5dc46f63ae6634ee0f2c130b1076e154995a
Shahu <prajwal.shahu@dxc.com>
2023-04-20 17:22:16 +0530

    update

---
+++ Csharp/StudentDetails/StudentDetails/Program.cs
@@ -0,0 +1,50 @@
+﻿Console.WriteLine("Enter Student ID");
+int ID = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter Student Name");
+string Name = Console.ReadLine();
+Console.WriteLine("Enter Student Age");
+int Age = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter Mark 1");
+int M1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter Mark 2");
+int M2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter Mark 3");
+int M3 = Convert.ToInt32(Console.ReadLine());
+double Total, Per;
+int overall_marks = 300;
+
+
+Total = M1 + M2 + M3;
+Per = (Total / overall_marks) * 100;
+
+Console.WriteLine("Your Total score is: " + Total);
+Console.WriteLine("Your Percentage is: " + Per);
+
+if (Per > 50)
+{
+    if(Per >= 90 && Per <=100)
+    {      
+        Console.WriteLine("your Grade is: A");
+        Console.WriteLine("Your Result For the exam is PASS");
+    }
+    else if (Per >= 80 && Per <= 89)
+    {
+        Console.WriteLine("your Grade is: B");
+        Console.WriteLine("Your Result For the exam is PASS");
+    }
+    else if (Per >= 60 && Per <= 79)
+    {
+        Console.WriteLine("your Grade is: C");
+        Console.WriteLine("Your Result For the exam is PASS");
+    }
+    else if (Per >= 50 && Per <= 59)
+    {
+        Console.WriteLine("your Grade is: D");
+        Console.WriteLine("Your Result For the exam is PASS");
+    }
+}
+else
+{
+    Console.WriteLine("your Grade is: E");
+    Console.WriteLine("Your Result For the exam is FAIL");
+}
---
+++ Csharp/Switchcase/Switchcase/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.WriteLine("Enter first number");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter Second number");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the operator (+,-,/,*,%)");
+char op = Convert.ToChar(Console.ReadLine());
+switch(op)
+{
+    case '+':
+        {
+            int res= num1 + num2;
+            Console.WriteLine("sum is: "+res);
+            break;
+        }
+    case '-':
+        {
+            int res = num1 - num2;
+            Console.WriteLine("difference is: "+res);
+            break;
+        }
+    case '/':
+        {
+            int res = num1 / num2;
+            Console.WriteLine("division is: "+res);
+            break;
+        }
+    case '*':
+        {
+            int res = num1 * num2;
+            Console.WriteLine("Multiple is: " + res);
+            break;
+        }
+    case '%':
+        {
+            int res = num1 % num2;
+            Console.WriteLine("Mode is: " + res);
+            break;
+        }
+    default:
+        {
+            Console.WriteLine("Invalid operator");
+            break;
+        }
+}
