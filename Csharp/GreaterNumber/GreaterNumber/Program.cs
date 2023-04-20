b24a9ee185ae2f3dfb1626a01e4136b754311344
Shahu <prajwal.shahu@dxc.com>
2023-04-20 12:27:49 +0530

    updated

---
+++ Csharp/GreaterNumber/GreaterNumber/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("enter first numbers");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("enter second number");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("enter Third number");
+int num3  = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2 && num1 > num3)
+{
+    Console.WriteLine("First number is greater");
+}
+else if (num2 > num1 && num2 > num3)
+{
+    Console.WriteLine("Second Number is greater");
+}
+else
+{
+    Console.WriteLine("Thrid number is greater");
+}
---
+++ Csharp/Vowel/Vowel/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter an Alphabet");
+char a = Convert.ToChar(Console.ReadLine());
+
+if (a == 'a' || a == 'i' ||  a == 'e' ||  a == 'o' ||  a == 'u')
+{
+    Console.WriteLine("Aplhabet is Vowel");
+}
+else
+{
+    Console.WriteLine("Alphabet is Consonant");
+}
+
